44f9541bc44640832c44a2cade01fc9bcca99832
Ajay-Mamundi <141619651+Ajay-Mamundi@users.noreply.github.com>
2024-02-13 15:54:55 +0530

    Add files via upload

---
+++ schema.cjs
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+
+// defining the schema
+const restaurantSchema = new mongoose.Schema({
+    areaName : {
+        type : String,
+    },
+    avgRating : {
+        type : String
+    },
+    costForTwo : {
+        type : String
+
+    },
+    cuisines : {
+        type : Array
+
+    },
+    name: {
+        type : String
+
+    }
+},{versionKey:false})
+
+// model
+const Restaurant= mongoose.model('restaurantlist', restaurantSchema)
+
+// Exporting the model
+const userSchema = new mongoose.Schema({
+    contact :{
+        type : Number,
+    },
+    email : {
+        type : String,
+    },
+    password : {
+        type : String,
+
+    }, userName : {
+        type : String,
+    }
+},{versionKey:false})
+
+// model
+const Users = mongoose.model('userDetail', userSchema)
+
+// Exporting the model
+module.exports = {Restaurant,Users}
